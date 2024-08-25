0e73dccb5224da027dc8606e9313e99352188f49
adebayo-glitch <pillarbasetech@gmail.com>
2024-08-25 11:18:51 -0400

    Add displayStockInformation

--- js/dom.js
+++ js/dom.js
@@ -17,9 +17,12 @@ export function displayStockInformation(stockInfo) {
     const stockInfoElement = document.getElementById('stockInfo');
     stockInfoElement.innerHTML = `
         <h2>${stockInfo['01. symbol']}</h2>
+         <p>Latest Trading Day: ${stockInfo['07. latest trading day']} </p>
         <p>Price: $${parseFloat(stockInfo['05. price']).toFixed(2)}</p>
+        <p>Previous Closing Price: $${parseFloat(stockInfo['08. previous close']).toFixed(2)}</p>
         <p>Change: ${stockInfo['09. change']} </p>
         <p>Change Percentage: (${stockInfo['10. change percent']})</p>
         <button id="addToWatchlistBtn">Add to Watchlist</button>
     `;
 }
+
