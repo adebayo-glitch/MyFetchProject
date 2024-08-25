1080d9faf77b5fbb75291e0684108ea55a2bcb31
adebayo-glitch <pillarbasetech@gmail.com>
2024-08-25 11:45:15 -0400

    import all the module

---
+++ js/index.js
@@ -0,0 +1,14 @@
+import {
+    displayStockInformation, 
+    displayStockWatchlistItem, 
+    getWatchlistItemInformation, 
+    removeWatchlist,
+} from './dom'
+import {
+    addStockToWatchlist, 
+    getStockInformation, 
+    removeStockFromWatchlist,
+    updateStockInformationNotes, 
+    updateStockTargetPrice
+} from './vintage-api'
+
