520a60a8896c75e0e8f47d8e10b87fa74901b4e5
adebayo-glitch <pillarbasetech@gmail.com>
2024-08-25 11:56:31 -0400

    Add handleAddStockToWatchlist function

--- js/index.js
+++ js/index.js
@@ -19,7 +19,22 @@ const stockSymbolInput = document.getElementById('stockSymbol');
 
 stockInformationSection.addEventListener('click', handleAddStockToWatchlist);
 
-
+async function handleAddStockToWatchlist(event) {
+    if (event.target.id === 'addToWatchlistBtn') {
+        const symbol = document.querySelector('#stockInfo h2').textContent;
+        try {
+            const result = await addStockToWatchlist(symbol);
+            if (result.success) {
+                displayStockWatchlistItem(result.data.id, symbol);
+                alert(`${symbol} added to watchlist`);
+            } else {
+                alert('Failed to add stock to watchlist');
+            }
+        } catch (error) {
+            alert('Error adding stock to watchlist. Please try again.');
+        }
+    }
+}
 
 
 
