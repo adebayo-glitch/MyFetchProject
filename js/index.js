9512ae8215abc13cbcfdcc3effaebeb9f2b386d6
adebayo-glitch <pillarbasetech@gmail.com>
2024-08-25 11:50:42 -0400

    add handleAddStockToWatchlist click event listener

--- js/index.js
+++ js/index.js
@@ -16,3 +16,12 @@ const stockInformationSection = document.getElementById('stockInfo');
 const searchButton = document.getElementById('searchBtn');
 const watchlistSection = document.getElementById('watchlist');
 const stockSymbolInput = document.getElementById('stockSymbol');
+
+stockInformationSection.addEventListener('click', handleAddStockToWatchlist);
+
+
+
+
+
+searchButton.addEventListener('click', handleStockSearch);
+watchlistSection.addEventListener('click', handleStockWatchlistActions);
