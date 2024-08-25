574ede4165271cb2eff1f31de76952ec26c8b219
adebayo-glitch <pillarbasetech@gmail.com>
2024-08-25 10:19:36 -0400

    add GetStockInformation Function

--- js/vintage-api.js
+++ js/vintage-api.js
@@ -1,3 +1,18 @@
 const API_KEY = 'DYU0TIW5D76590CG';
 const ALPHA_VANTAGE_URL = 'https://www.alphavantage.co/query';
 const JSON_PLACEHOLDER_URL = 'https://jsonplaceholder.typicode.com';
+
+
+
+
+export async function getStockInformation(symbol) {
+    try {
+        const response = await axios.get(
+            `${ALPHA_VANTAGE_URL}?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${API_KEY}`
+        );
+        return response.data['Global Quote'];
+    } catch (error) {
+        console.error('Error fetching stock info:', error);
+        throw error;
+    }
+}
